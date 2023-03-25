a2f0014591a760d30c864ac74ebc1594f40c9f10
Héctor Chocobar-Torrejón <hector@chocobar.net>
2023-03-25 13:42:55 +0000

    feature: new Home

--- src/js/layout.jsx
+++ src/js/layout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
-import { Home } from "./views/home.jsx";
+import { Home } from "./views/Home.jsx";
 import { People } from "./views/People.jsx";
 import { Planets } from "./views/Planets.jsx";
 import { PlanetsDetails } from "./views/PlanetsDetails.jsx";
@@ -20,7 +20,7 @@ import { Footer } from "./component/footer.jsx";
 
 //create your first component
 const Layout = () => {
-	//the basename is used when your project is published in a subdirectory and not in the root of the domain
+	// the basename is used when your project is published in a subdirectory and not in the root of the domain
 	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
 	const basename = process.env.BASENAME || "";
 
